9818c992ece77f3cc7d8a1d2c2b8b45a48f57796
aakash05mrt <88130639+aakash05mrt@users.noreply.github.com>
2021-10-24 19:30:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo ", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Warriyo ", filePath: "songs/1.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Cielo ", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "DEAF KEV", filePath: "songs/3.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Different Heaven ", filePath: "songs/4.mp3", coverPath: "covers/9.jpg"},
     {songName: "Janji-Heroes", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba ", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan ", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
