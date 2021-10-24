03d5cbc7b812fdea0ae49715d05094b9a3325c51
aakash05mrt <88130639+aakash05mrt@users.noreply.github.com>
2021-10-24 19:04:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo ", filePath: "songs/1.mp3", coverPath: "covers/1.png"},
+    {songName: "Warriyo ", filePath: "songs/1.mp3", coverPath: "covers/1.PNG"},
     {songName: "Cielo ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "DEAF KEV", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Different Heaven ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
